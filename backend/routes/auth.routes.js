96f92a12cf3f2a470cd24b04f79c659a0762589f
estephany <estephany-rualesm@unilibre.edu.co>
2026-05-01 18:29:36 -0500

    feat: página de gestión de Usuarios (solo admin)

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -8,5 +8,6 @@ const checkRole     = require('../middleware/checkRole');
 router.post('/login',    login);
 router.post('/registro', verifyToken, checkRole('admin'), registro); // solo admin crea usuarios
 router.get('/perfil',    verifyToken, perfil);
+router.get('/usuarios', verifyToken, checkRole('admin'), listarUsuarios);
 
 module.exports = router;
